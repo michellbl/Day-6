0ccb1803e4de5d5d82273e32fa22d7ce8781e9fe
Michell Bezerra de Lima <michellblima@gmail.com>
2022-01-12 18:25:11 -0300

    First For project
    The project start an For from zero to ten, with sleep for 1s, them at the end its show fireworks.

---
+++ exercicio1.py
@@ -0,0 +1,24 @@
+#Faça um programa que mostre na tela
+#uma contagem regressiva para o estouro de fogos
+#de artificio, indo de 10 até 0, com uma pausa de 1
+#segundo entre eles.
+
+import time
+
+for n in range(0,11):
+    print(n)
+    time.sleep(1)
+print('''
+                                 .''.
+       .''.             *''*    :_\/_:     . 
+      :_\/_:   .    .:.*_\/_*   : /\ :  .'.:.'.
+  .''.: /\ : _\(/_  ':'* /\ *  : '..'.  -=:o:=-
+ :_\/_:'.:::. /)\*''*  .|.* '.\'/.'_\(/_'.':'.'
+ : /\ : :::::  '*_\/_* | |  -= o =- /)\    '  *
+  '..'  ':::'   * /\ * |'|  .'/.\'.  '._____
+      *        __*..* |  |     :      |.   |' .---"|
+       _*   .-'   '-. |  |     .--'|  ||   | _|    |
+    .-'|  _.|  |    ||   '-__  |   |  |    ||      |
+    |' | |.    |    ||       | |   |  |    ||      |
+ ___|  '-'     '    ""       '-'   '-.'    '`      |____
+''')
